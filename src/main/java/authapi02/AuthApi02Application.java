835b505b52ec98478957abdee9414ccc52c65d46
Fab-2727 <fabriziolionelsosa27@gmail.com>
2021-12-25 20:35:46 -0300

    Init project

---
+++ src/main/java/authapi02/AuthApi02Application.java
@@ -0,0 +1,13 @@
+package authapi02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApi02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApi02Application.class, args);
+	}
+
+}
---
+++ src/test/java/authapi02/AuthApi02ApplicationTests.java
@@ -0,0 +1,13 @@
+package authapi02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApi02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
