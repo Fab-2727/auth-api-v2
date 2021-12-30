c3d651121d4fd592e4298921d74c6eac68ee2578
Fab-2727 <fabriziolionelsosa27@gmail.com>
2021-12-30 17:39:35 -0300

    Add logging methods

--- src/main/java/authapi02/AuthApi02Application.java
+++ src/main/java/authapi02/AuthApi02Application.java
@@ -9,5 +9,39 @@ public class AuthApi02Application {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApi02Application.class, args);
 	}
+	
+	public static String getExceptionDescriptionForLogging(Throwable e) {
+		if (e == null) {
+			return "";
+		}
+		String exceptionDescription = "";
+		exceptionDescription = getExceptionClassAndMessage(e) + " " + getExceptionInfo(e);
 
+		String causedBy = "";
+		if (e.getCause() != null) {
+			causedBy = "[Caused by : " + getExceptionClassAndMessage(e.getCause()) + " " + getExceptionInfo(e.getCause()) + " ]";
+		}
+		return exceptionDescription + causedBy;
+		
+	}
+	
+	public static String getExceptionClassAndMessage(Throwable e) {
+		String exceptionClassAndMessage = e.getClass().getSimpleName() + " : " + e.getMessage();
+		return exceptionClassAndMessage;
+	}
+
+	public static String getExceptionInfo(Throwable e) {
+		if (e == null) {
+			return "";
+		}
+		String exceptionCause = "";
+		try {
+			exceptionCause = "[at: " + e.getStackTrace()[0].getFileName() + "#" + e.getStackTrace()[0].getMethodName() + ":" + e.getStackTrace()[0].getLineNumber() + "]";
+			return exceptionCause;
+		} catch (Exception e2) {
+			return "";
+		}
+		
+	}
+	
 }
