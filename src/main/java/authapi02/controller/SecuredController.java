cd8266c11a5679b845c523c60810be8d4062c9b0
Fab-2727 <fabriziolionelsosa27@gmail.com>
2022-01-04 22:49:02 -0300

    Add @secured to controllers

--- src/main/java/authapi02/controller/SecuredController.java
+++ src/main/java/authapi02/controller/SecuredController.java
@@ -1,7 +1,7 @@
 package authapi02.controller;
 
-
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/secure")
 public class SecuredController {
 
-	
+	@Secured({ "ROLE_USER", "ROLE_ADMIN" })
 	@GetMapping(path = "/greeting")
 	public ResponseEntity<Object> secureEndpoint () {
-		return ResponseEntity.ok("You have reached a secure endpoint.");
+		return ResponseEntity.ok("[ROLE_USER_ENDPOINT] You have reached a secure endpoint.");
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@GetMapping(path = "/greeting-two")
 	public ResponseEntity<Object> secureEndpointAdmin () {
-		return ResponseEntity.ok("You have reached a secure endpoint.");
+		return ResponseEntity.ok("[ROLE_ADMIN_ENDPOINT] You have reached a secure endpoint.");
 	}
 	
 	
--- src/main/java/authapi02/controller/UserController.java
+++ src/main/java/authapi02/controller/UserController.java
@@ -3,6 +3,7 @@ package authapi02.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,10 @@ public class UserController {
 	@Autowired
 	CustomUserService customUserService;
 	
+	/**
+	 * Endpoint only available for 'ADMIN' user.
+	 */
+	@Secured("ROLE_ADMIN")
 	@PostMapping(path = "/register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> registerUser(@RequestBody CustomUser user) {
 		System.out.println("Request received: " + user.toString());
